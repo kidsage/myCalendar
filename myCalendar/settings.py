894b528d488a5b16d12f941e70ff69c98e62401c
kidsage <ruhyun92@gmail.com>
2023-05-05 02:07:14 +0900

    Feat: edit calendar basic features

--- myCalendar/settings.py
+++ myCalendar/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k^(+$86pb928q&u&&s0q44^i#2lue0u(9m$u49%ct40z)56vx^'
+load_dotenv()
+SECRET_KEY: str = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
